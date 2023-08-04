bcfaf57d0eceb3ba3dc418a5b2fd62133883713b
Tootin0730 <yisan1004@gmail.com>
2023-08-04 12:30:44 +0900

    2.3 Recap

--- src/server.js
+++ src/server.js
@@ -1,23 +1,21 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
-
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
-
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
-      done();
-    }, 10000);
+      done("hello from the backend");
+    }, 15000);
   });
 });
 
@@ -41,6 +39,5 @@ wss.on("connection", (socket) => {
     }
   });
 }); */
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
