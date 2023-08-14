b32f110c20b922e678c231c9ad4a25d55b666841
Tootin0730 <yisan1004@gmail.com>
2023-08-14 16:40:23 +0900

    2.8 Room Count pt.1

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
+
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
 
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
